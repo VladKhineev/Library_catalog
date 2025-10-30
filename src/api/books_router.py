de4f6276e4491aa7448db2bfa3c1fa89c95e4f70
VladKhineev <vlad.khineev@gmail.com>
2025-10-30 17:42:41 +0300

    (dev): add handle_error to books_routers

--- src/api/books_router.py
+++ src/api/books_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from src.core import config
+from src.core.decorators import handle_error
 
 from src.managers.book_manager import BookManager
 from src.managers.enrichment_manager import BookEnrichmentManager
@@ -40,27 +41,32 @@ def get_enrichment_manager(source: Repo) -> BookEnrichmentManager:
     api = OpenLibraryAPI()
     return BookEnrichmentManager(manager, api)
 
+@handle_error(default_return=[], msg='Ошибка при получении списка книг')
 @router.get('/')
 def get_books(manager: BookManager = Depends(get_book_manager)):
     return manager.get_books()
 
+@handle_error(default_return=[], msg='Ошибка при добавлении книги')
 @router.post('/')
 def add_book(book: Book, manager: BookManager = Depends(get_book_manager)):
     return manager.add_book(book)
 
-
+@handle_error(default_return=[], msg='Ошибка при получении книги')
 @router.get('/{book_id}')
 def get_book(book_id: int, manager: BookManager = Depends(get_book_manager)):
     return manager.get_book(book_id)
 
+@handle_error(default_return=[], msg='Ошибка при обновлении книги')
 @router.put('/')
 def update_book(book: Book, manager: BookManager = Depends(get_book_manager)):
     return manager.update_book(book)
 
+@handle_error(default_return=[], msg='Ошибка при удалении книги')
 @router.delete('/{book_id}')
 def delete_book(book_id: int, manager: BookManager = Depends(get_book_manager)):
     return manager.delete_book(book_id)
 
+@handle_error(default_return=[], msg='Ошибка при добавлении книги')
 @router.post('/enriched/')
 def create_book_enriched(book: Book, manager: BookEnrichmentManager = Depends(get_enrichment_manager)):
     return manager.add_with_api(book)
