0c571a49cb823e572161ff620aa1285e81445366
VladKhineev <vlad.khineev@gmail.com>
2025-10-31 15:49:39 +0300

    fix config

--- src/core/config.py
+++ src/core/config.py
@@ -1,3 +1,16 @@
-POSTGRES_URL = 'postgresql://postgres:postgres@localhost:5432/Library'
-MASTER_KEY = '$2a$10$v/qfQsVRSLYVUUe7wBPp5ONexSDmwvuqchMBwBZzEDSJErk24DW4O'
-BIN_ID = '69008a2a43b1c97be986cdc7'
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False
+    )
+
+    POSTGRES_URL: str
+    JSONBIN_MASTER_KEY: str
+    JSONBIN_BIN_ID: str
+
+
+settings = Settings()  # Через Depends желателньо прокинуть где будешь юзать или создать def get_settings()
