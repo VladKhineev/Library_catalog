ea1395d0bb91e4bd3e23e1e49e612229853547d8
VladKhineev <vlad.khineev@gmail.com>
2025-11-05 13:58:19 +0300

    (fix): decorators without self

--- src/core/decorators.py
+++ src/core/decorators.py
@@ -7,7 +7,7 @@ import src.core.exceptions as exception
 
 
 def handle_error(
-    default_return=None, http_error: int | None = None, msg: str | None = None
+    default_return=None, msg: str | None = None
 ):
     """
     Универсальный декоратор для логирования ошибок.
@@ -16,24 +16,15 @@ def handle_error(
 
     def decorator(func):
         @functools.wraps(func)
-        async def async_wrapper(self, *args, **kwargs):
+        async def async_wrapper(*args, **kwargs):
             try:
-                return await func(self, *args, **kwargs)
+                return await func(*args, **kwargs)
             except exception.BookNotFoundError as e:
-                log = getattr(self, "logger", logger)
-                log.warning(f"[404] {e}")
-                raise HTTPException(status_code=404, detail=str(e))
-            except AttributeError as e:
-                log = getattr(self, "logger", logger)
-                log.warning(f"[404] {e}")
                 raise HTTPException(status_code=404, detail=str(e))
             except Exception as e:
-                log = getattr(self, 'logger', logger)
-                log.exception(
-                    f"Ошибка в {self.__class__.__name__}.{func.__name__}: {e}"
-                )
-                if http_error:
-                    raise HTTPException(status_code=http_error, detail=msg or str(e))
+                logger.exception(f"Error in {func.__name__}: {e}")
+                if msg:
+                    raise HTTPException(status_code=500, detail=msg)
                 return default_return
 
         return async_wrapper
